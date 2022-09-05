354af0367c2cd005154ab2f8bfed0052869b8876
fabrizia2 <melvinrenish56@gmail.com>
2022-09-05 06:42:41 -0700

    heart shape

---
+++ love.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int ren, king;
+
+	for (ren = 1; ren <= 3; ren++)
+	{
+		for (king = 1; king <= 17; king++)
+		{
+			if ((king >= 4-ren && king <= 5 + ren) || king >= 13 - ren && king <= 14 + ren)
+				printf("@");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+	for (ren = 1; ren <= 9; ren++)
+	{
+		for(king = 1; king <= 17; king++)
+		{
+			if (king >= ren && king <= 18 - ren)
+				printf("@");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+}
